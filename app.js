28d7425e6d069bc2cdcc4e76ad9ba1c158f0779e
RicardoArcos1 <ricardoarcos98@hotmail.com>
2023-10-26 23:06:04 -0500

    Key

--- app.js
+++ app.js
@@ -1,43 +1,50 @@
 // app.js
+require('dotenv').config();
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const port = 3000;
 const app = express();
 
-app.use(express.json()); // Para poder parsear JSON en el cuerpo de las solicitudes
+app.use(express.json());
 
-// Almacenamiento de tareas
-global.tasks = [
-    {
-        id: "123456",
-        isCompleted: false,
-        description: "study phyton"
-    },
-    {
-        id: "123457",
-        isCompleted: true,
-        description: "study sql"
-    },
-    {
-        id: "123458",
-        isCompleted: false,
-        description: "study java"
-    }
+// Usuarios predefinidos
+const users = [
+  { id: 1, username: 'user1', password: 'password1' },
+  { id: 2, username: 'user2', password: 'password2' },
 ];
 
-// Ruta para obtener la lista de tareas
-app.get('/tasks', (req, res) => {
-    res.send(global.tasks);
+// Ruta de login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (user) {
+    const accessToken = jwt.sign({ id: user.id, username: user.username }, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken });
+  } else {
+    res.status(401).send('Usuario o contraseña incorrectos');
+  }
 });
 
-// Ruta para ver la lista de tareas
-app.use('/list-view', require('./list-view-router'));
+// Middleware para validar el token JWT
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
-// Ruta para editar la lista de tareas
-app.use('/list-edit', require('./list-edit-router'));
+  if (!token) return res.status(401).send('Token no proporcionado');
 
-// Iniciar el servidor
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(403).send('Token no válido');
+    req.user = user;
+    next();
+  });
+}
+
+// Ruta protegida
+app.get('/protected', authenticateToken, (req, res) => {
+  res.send('Acceso autorizado');
+});
+
 app.listen(port, () => {
-    console.log(`El servidor esta corriendo en el puerto ${port}`);
+  console.log(`El servidor esta corriendo en el puerto ${port}`);
 });
-
-
